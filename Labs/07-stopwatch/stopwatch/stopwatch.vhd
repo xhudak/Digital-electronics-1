279c9c687d789ce7d720e04e3aa728f4ccd1120e
xhudak <60730015+xhudak@users.noreply.github.com>
2020-03-28 00:51:17 +0100

    Add files via upload

---
+++ Labs/07-stopwatch/stopwatch/stopwatch.vhd
@@ -0,0 +1,83 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_unsigned.all;
+
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity stopwatch is
+
+			port	(
+					--Inputs
+					
+					clk_i : in std_logic; 		
+					srst_n_i : in std_logic;  	
+					ce_100Hz_i : in std_logic; 
+					cnt_en_i : in std_logic; 	
+						
+					--Outputs
+					
+					sec_h_o : out std_logic_vector (3 downto 0);	
+					sec_l_o : out std_logic_vector (3 downto 0);	
+					hth_h_o : out std_logic_vector (3 downto 0);	
+					hth_l_o : out std_logic_vector (3 downto 0)	
+				);
+				
+end entity stopwatch;
+
+architecture Behavioral of stopwatch is
+	 
+	 signal sec_h : std_logic_vector (3 downto 0) := "0000";
+	 signal sec_l : std_logic_vector (3 downto 0) := "0000";
+	 signal hth_h : std_logic_vector (3 downto 0) := "0000";
+	 signal hth_l : std_logic_vector (3 downto 0) := "0000";
+
+begin
+			sec_h_o <= sec_h;
+			sec_l_o <= sec_l;
+			hth_h_o <= hth_h;
+			hth_l_o <= hth_l;
+
+	stopwatch_pocitadlo : process (clk_i) 
+		begin 
+			if rising_edge (clk_i) then
+					if srst_n_i = '0' then 
+						sec_h <= "0000";
+						sec_l <= "0000";
+						hth_h <= "0000";
+						hth_l <= "0000";
+							
+					elsif cnt_en_i = '1' then
+							
+							if sec_h  = "0101" and sec_l = "1001" and hth_h = "1001" and hth_l = "1001" then -- 59 99 --
+								sec_h <= "0000";
+								sec_l <= "0000";
+								hth_h <= "0000";
+								hth_l <= "0000";
+										
+							elsif sec_l = "1001" and hth_h = "1001" and hth_l = "1001" then -- 09 99 --
+								sec_h <= sec_h + 1;
+								sec_l <= "0000";
+								hth_h <= "0000";
+								hth_l <= "0000";
+											
+							elsif hth_h = "1001" and hth_l = "1001" then  -- 00 99 --
+								sec_l <= sec_l + 1;
+								hth_h <= "0000";
+								hth_l <= "0000";
+						
+							elsif hth_l = "1001" then  	-- 00 09 --
+								hth_h <= hth_h + 1;
+								hth_l <= "0000";
+					else 
+						hth_l <= hth_l +1 ;
+						
+						end if;
+					
+					end if;
+				
+			end if;
+									
+		end process stopwatch_pocitadlo;
+										
+end architecture Behavioral;
---
+++ Labs/07-stopwatch/stopwatch/stopwatch_TestBench.vhd
@@ -0,0 +1,88 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY stopwatch_TestBench IS
+END stopwatch_TestBench;
+ 
+ARCHITECTURE behavior OF stopwatch_TestBench IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT stopwatch
+    PORT(
+         clk_i : IN  std_logic;
+         srst_n_i : IN  std_logic;
+         ce_100Hz_i : IN  std_logic;
+         cnt_en_i : IN  std_logic;
+         sec_h_o : OUT  std_logic_vector(3 downto 0);
+         sec_l_o : OUT  std_logic_vector(3 downto 0);
+         hth_h_o : OUT  std_logic_vector(3 downto 0);
+         hth_l_o : OUT  std_logic_vector(3 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk_i : std_logic := '0';
+   signal srst_n_i : std_logic := '0';
+   signal ce_100Hz_i : std_logic := '0';
+   signal cnt_en_i : std_logic := '0';
+
+ 	--Outputs
+   signal sec_h_o : std_logic_vector(3 downto 0);
+   signal sec_l_o : std_logic_vector(3 downto 0);
+   signal hth_h_o : std_logic_vector(3 downto 0);
+   signal hth_l_o : std_logic_vector(3 downto 0);
+
+   -- Clock period definitions
+   constant clk_i_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: stopwatch PORT MAP (
+          clk_i => clk_i,
+          srst_n_i => srst_n_i,
+          ce_100Hz_i => ce_100Hz_i,
+          cnt_en_i => cnt_en_i,
+          sec_h_o => sec_h_o,
+          sec_l_o => sec_l_o,
+          hth_h_o => hth_h_o,
+          hth_l_o => hth_l_o
+        );
+
+   -- Clock process definitions
+   clk_i_process :process
+   begin
+		clk_i <= '0';
+		wait for clk_i_period/2;
+		clk_i <= '1';
+		wait for clk_i_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   tim_proc: process
+		begin		
+			ce_100Hz_i <= '0';
+			cnt_en_i <= '1';
+			srst_n_i <= '1'; 
+			wait for clk_i_period;
+			srst_n_i <= '0'; 
+			wait for clk_i_period;
+			srst_n_i <= '1'; 
+			
+			for i in 0 to 70000 loop
+				ce_100Hz_i <= '1';
+				wait for clk_i_period*1;			
+				ce_100Hz_i <= '0';
+				wait for clk_i_period*99;
+			 
+		end loop;
+   end process;
+
+END;
